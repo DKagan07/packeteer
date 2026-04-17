bbe2295b714f357ac56c11a3fc56c64896ed6877
David Kagan <david.kagan07@gmail.com>
2026-04-16 21:08:31 -0400

    wip(Phase5): wip for working with abuseipdb
    NOTE: this requires to have the api key in the $HOME/.packeteer.yaml file
    Check root.go

---
+++ internal/api/abuse_ipdb.go
@@ -0,0 +1,97 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+const abuseipdb_root = "https://api.abuseipdb.com/api/v2/"
+const abuseipdb = "abuseipdb_api_key"
+
+type Reports struct {
+	ReportedAt          time.Time `json:"reportedAt"`
+	Comment             string    `json:"comment"`
+	Categories          []int     `json:"categories"`
+	ReporterId          int       `json:"reporterId"`
+	ReporterCountryCode string    `json:"reporterCountryCode"`
+	ReporterCountryName string    `json:"reporterCountryName"`
+}
+
+type RootData struct {
+	IpAddress            string        `json:"ipAddress"`
+	IsPublic             bool          `json:"isPublic"`
+	IpVersion            int           `json:"ipVersion"`
+	IsWhitelisted        bool          `json:"isWhitelisted"`
+	AbuseConfidenceScore int           `json:"abuseConfidenceScore"`
+	CountryCode          string        `json:"countryCode"`
+	CountryName          string        `json:"countryName"`
+	UsageType            string        `json:"usageType"`
+	Isp                  string        `json:"isp"`
+	Domain               string        `json:"domain"`
+	Hostnames            []interface{} `json:"hostnames"`
+	IsTor                bool          `json:"isTor"`
+	TotalReports         int           `json:"totalReports"`
+	NumDistinctUsers     int           `json:"numDistinctUsers"`
+	LastReportedAt       time.Time     `json:"lastReportedAt"`
+	Reports              []Reports     `json:"reports"`
+}
+
+type AbuseIpDbResponse struct {
+	Data RootData `json:"data"`
+}
+
+// getApiKey
+func getApiKey() string {
+	return viper.GetString(abuseipdb)
+}
+
+// CheckIP will call the AbuseIPdb service to get information about a specific
+// IP address
+func CheckIP(ip string) {
+	checkUrl := fmt.Sprintf("%s%s", abuseipdb_root, "check")
+	rawUrl, err := url.Parse(checkUrl)
+	if err != nil {
+		return
+	}
+
+	values := url.Values{}
+	values.Set("ipAddress", ip)
+
+	rawUrl.RawQuery = values.Encode()
+
+	checkUrl = rawUrl.String()
+	fmt.Println("checkUrl:", checkUrl)
+
+	req, err := http.NewRequest(http.MethodGet, checkUrl, nil)
+	if err != nil {
+		return
+	}
+
+	req.Header.Add("Key", getApiKey())
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	var abuseIpDbResponse *AbuseIpDbResponse
+	if err := json.Unmarshal(bodyBytes, abuseIpDbResponse); err != nil {
+		return
+	}
+
+	fmt.Printf("%+v\n", abuseIpDbResponse)
+
+}
---
+++ internal/api/abuse_ipdb_test.go
@@ -0,0 +1,9 @@
+package api
+
+import (
+	"testing"
+)
+
+func TestCheck(t *testing.T) {
+	CheckIP("192.168.1.1")
+}
